36bed9bec1ccf348a2d25d5fa477beb5c627774f
Maksym1910 <55399296+Maksym1910@users.noreply.github.com>
2020-08-21 17:47:13 +0300

    added eslint configure / fixed syntax problems

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-undef */
 'use strict';
 
-$(window).on('load', function () {
+$(window).on('load', () => {
 
-    // Vide.js - video background
-    $('#header').vide('./video/cover', {
-        bgColor: '#3C494C'
-    })
+  // Vide.js - video background
+  $('#header').vide('./video/cover', {
+    bgColor: '#3C494C'
+  });
 
-})
+});
